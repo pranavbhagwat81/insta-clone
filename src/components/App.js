6041724e9dba233b10af55c0669ec39725018362
pranavbhagwat92 <pranavbhagwat81@gmail.com>
2020-07-30 13:42:50 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './Header'
+import '../index.css'
+import Post from './Post'
+
+function App() {
+    return (
+        <div className='app'>
+            <div className = 'app__header'>
+                <Header/>
+            </div>
+            <div className='app__body'>
+                <Post/>
+            </div>
+        </div>
+
+    )
+}
+
+export default App
---
+++ src/components/Post.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../index.css'
+
+function Post() {
+    return (
+        <div className='post'>
+            <h3>Model</h3>
+            <img className='post__image'
+            src="https://pixabay.com/get/57e2d2474a57aa14f1dc8460da2932771036d8e7505777_640.jpg"
+            alt='post_image'
+            />
+        </div>
+    )
+}
+
+export default Post 
